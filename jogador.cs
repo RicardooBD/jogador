9269b3eefc59c0fa0ba1b44964898cfb1f67cf6f
RicardooBD <112703146+RicardooBD@users.noreply.github.com>
2023-08-09 11:51:29 -0300

    ricardo

---
+++ jogador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class jogador : Program
+
+    {
+        string nome;
+        int idade;
+        int drible;
+        int velocidade;
+        int chute;
+        int forca;
+
+        public void correr()
+        {
+            int aceleracao = forca + velocidade;
+        }
+        public int chutar()
+        {
+            int potenciachute  = forca * chute;
+            return potenciachute;
+        }
+        public double tocar()
+        {
+            double tocar = forca / 3 +  chute / 3;
+            return tocar;
+        }
+        public double interceptar()
+        {
+            double roubar = velocidade * velocidade + forca / 2;
+            return roubar;
+        }
+        public int driblar()
+        {
+            int driblar = drible  + velocidade * velocidade;
+            return driblar;
+        }
+    }
+}
